227e1e13c4ff5ea990571221ae8932c6712b3d01
Sooyoung Lee <dannylisa67@gmail.com>
2023-04-15 01:45:19 +0900

    initial setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RecoilRoot } from "recoil";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
-)
+  </RecoilRoot>
+);
