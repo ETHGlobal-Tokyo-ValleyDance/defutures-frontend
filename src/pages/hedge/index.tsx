8122a04beee58cdb6b7d0f113e558f654c4cd66d
mangming2 <leejiho35@gmail.com>
2023-04-15 15:12:27 +0900

    Update index.tsx

--- src/pages/hedge/index.tsx
+++ src/pages/hedge/index.tsx
@@ -1,218 +1,60 @@
-import { TokenSelector } from "components/common/TokenSelector";
 import { useHedge } from "./hedge.service";
 import { NumberInput } from "components/common/NumberInput";
-import { cn } from "utils";
 
-const MIN_SPOT_PERCENT = 60;
 const Hedge = () => {
   const {
     totalAmount,
     spotPercent,
-    spotAmount,
     maxSpotPercent,
     marginRatio,
     tolerance,
     minMarginBps,
-    totalSupply,
-    tokenList,
-    baseToken,
-    farmToken,
-    setBaseSymbol,
-    setFarmSymbol,
     setTotalAmount,
     onChangeSpotPercent,
-  } = useHedge(MIN_SPOT_PERCENT);
-
-  const disabled = +tolerance < 0;
-  const baseTokenBalance = 0;
-  // TODO
-  const dexName = "Uniswap V2";
+  } = useHedge();
 
   return (
-    <div className="px-24 py-12 flex">
-      <div className="px-10 py-6 flex flex-col flex-[2] border-r border-r-neutral-200">
-        {/* DESCRIPTION */}
-        <div className=" bg-primary-50 p-5 rounded-lg">
-          <p className="leading-[22px]">
-            To invest in <b className="text-primary-700">Defutures</b>, you
-            should deposit a margin according to the&nbsp;
-            <b className="text-primary-700">Minimum Margin Ratio</b>. If long
-            positions fall and short positions rise beyond the&nbsp;
-            <b className="text-primary-700">Tolerance</b>, your position may be
-            subject to liquidation.
-          </p>
-        </div>
+    <div className="flex bg-primary-50 w-[600px] h-[500px] m-auto mt-10 mb-10 items-center justify-center p-8 rounded shadow-xl">
+      <div className="flex flex-col w-[400px] ">
+        <p className="font-semibold  mb-3">Total Amount</p>
+        <NumberInput value={totalAmount} onChange={setTotalAmount} />
 
-        <p className="font-bold text-xl mt-6">
-          Select a token to invest in DEX.
-        </p>
-        <hr className="mt-2" />
-        <div className="flex my-4">
-          <TokenSelector
-            tokenList={tokenList}
-            selected={baseToken.symbol}
-            onSelect={setBaseSymbol}
-          />
-        </div>
-
-        <p className="font-bold text-xl mt-4">
-          Select a pair token to invest with the above.
-        </p>
-        <hr className="mt-2" />
-        <div className="flex my-4">
-          <TokenSelector
-            tokenList={tokenList}
-            selected={farmToken.symbol}
-            onSelect={setFarmSymbol}
-          />
-        </div>
-
-        <p className="font-bold text-xl mt-4">
-          Enter the amount of BASE Token you'd like to invest.
-        </p>
-        <hr className="mt-2" />
-        <NumberInput
-          className="mt-4"
-          value={totalAmount}
-          onChange={setTotalAmount}
-          left={
-            <div className="px-5 h-full flex-center border rounded-lg bg-neutral-200">
-              <p className="font-semibold pt-1">{baseToken.symbol}</p>
-            </div>
-          }
-          right={
-            <button className="btn btn-primary h-full flex-center">Max</button>
-          }
+        <p className="mt-4"> Hedge Ratio </p>
+        <input
+          className="mt-4 accent-primary-300 focus:accent-primary-500 border-none bg-primary-700"
+          type="range"
+          min="60"
+          step={0.1}
+          max={maxSpotPercent}
+          value={spotPercent}
+          onChange={onChangeSpotPercent}
         />
+        <div className="flex justify-between">
+          <p>{50}</p>
+          <p>{spotPercent}</p>
+          <p>{maxSpotPercent}</p>
+        </div>
 
-        {/* TODO */}
-        <p className="text-right mt-2 pr-1 text-neutral-600">
-          Your balance: {baseTokenBalance || "0.0"}
-        </p>
-      </div>
+        <hr className="my-4" />
 
-      {/* RIGHT SIDE */}
-      <div className="px-10 py-4 flex justify-center flex-[3] ">
-        <div className="rounded-2xl shadow bg-white p-9 py-8 flex flex-1 flex-col">
-          <p className=" text-neutral-400 mb-3">
-            FUTURE NFT #{totalSupply + 1}
-          </p>
-          <div className="flex items-center">
-            <span className="chip chip-blue">Base</span>
-            <p className="text-2xl mx-3 pt-0.5 font-semibold">
-              {baseToken.symbol} + {farmToken.symbol}
-            </p>
-            <span className="chip chip-primary">Farm</span>
+        <div className="mt-2 flex justify-between text-primary-800">
+          <p className="font-semibold">Spot vs. Hedge Ratio</p>
+          <div className="flex">
+            <p className=" text-red-600">{spotPercent} </p> :{" "}
+            <p className=" text-blue-600"> {(1000 - 10 * +spotPercent) / 10}</p>
           </div>
-
-          <hr className="my-4" />
-
-          <label
-            htmlFor="spot-range"
-            className="block mb-2 text-lg font-semibold text-gray-900 dark:text-white"
-          >
-            Spot - Hedge Ratio
-          </label>
-          <div className="flex justify-between mb-3">
-            <div className="chip-sm chip-purple">Min {MIN_SPOT_PERCENT}%</div>
-            <div className="chip-sm chip-purple">Max 100%</div>
-          </div>
-          <input
-            id="spot-range"
-            className="rounded-lg overflow-hidden appearance-none bg-primary-100 h-3 w-128"
-            type="range"
-            step={0.1}
-            min={MIN_SPOT_PERCENT}
-            max="100"
-            value={spotPercent}
-            onChange={onChangeSpotPercent}
-          />
-
-          {/* INFOS */}
-          <div className=" bg-primary-50 p-4 rounded-lg my-3">
-            <p className="leading-[26px] text-lg">
-              Your investment funds of
-              <b className="text-primary-700"> {totalAmount} USDC </b>
-              will be divided as follows:
-              <br />•{" "}
-              <b className="text-primary-700">
-                {spotAmount} {baseToken.symbol}{" "}
-              </b>
-              will be allocated to {dexName}
-              <br />• remaining{" "}
-              <b className="text-primary-700">
-                {(1000 * +totalAmount - spotAmount * 1000) / 1000}{" "}
-                {baseToken.symbol}
-              </b>{" "}
-              will be invested in futures for hedging position.
-            </p>
-          </div>
-
-          <hr className="my-3" />
-          {/* RECEIPT PREVIEW */}
-          <div className="flex flex-col gap-y-1.5">
-            {/* MARGIN RATIO */}
-            <div
-              className={cn(
-                "flex flex-1 gap-4",
-                marginRatio < minMarginBps / 100 && "text-red-600"
-              )}
-            >
-              <div className="flex flex-1 justify-between">
-                <p className="font-semibold">Current Margin Ratio</p>
-                <p> {marginRatio}%</p>
-              </div>
-              <p>:</p>
-              <p className="flex-1 text-neutral-500">
-                Your {farmToken.symbol} equity compared to strike amount
-              </p>
-            </div>
-
-            {/* MINIMUM MARGIN RATIO */}
-            <div className="flex flex-1 gap-4">
-              <div className="flex flex-1 justify-between">
-                <p className="font-semibold">Minimum Margin Ratio</p>
-                <p> {minMarginBps / 100}%</p>
-              </div>
-              <p>:</p>
-              <p className="flex-1 text-neutral-500">
-                Min margin ratio required to maintain position.
-              </p>
-            </div>
-
-            {/* TOLERANCE */}
-            <div className="flex flex-1 gap-4">
-              <div className="flex flex-1 justify-between">
-                <p className="font-semibold">Tolerance</p>
-                <p> {tolerance >= 100 || tolerance < 0 ? "-" : tolerance}%</p>
-              </div>
-              <p>:</p>
-              <p className="flex-1 text-neutral-500">
-                The volatility of price fluctuations between paired tokens that
-                can be tolerated.
-              </p>
-            </div>
-          </div>
-
-          {/* WARNING */}
-          <div className=" bg-yellow-50 p-4 rounded-lg my-4">
-            <p>
-              Investing a small percentage of funds in position hedging{" "}
-              <b className="text-yellow-800">
-                may expose the position to liquidation risk
-              </b>{" "}
-              even with minor price fluctuations, while{" "}
-              <b className="text-yellow-800">
-                overinvesting in hedging may limit potential returns.
-              </b>
-            </p>
-          </div>
-
-          <div className="flex-1 mt-2 flex-center">
-            <button className="btn-lg btn-primary">
-              Start to invest Hedged position 🎉
-            </button>
-          </div>
+        </div>
+        <div className="mt-2 flex justify-between text-primary-800">
+          <p className="font-semibold">Current Margin Ratio</p>
+          <p>{marginRatio}%</p>
+        </div>
+        <div className="mt-2 flex justify-between text-primary-800">
+          <p className="font-semibold">Tolerance to volatility</p>
+          <p>{tolerance >= 100 ? ">100" : tolerance}%</p>
+        </div>
+        <div className="mt-2 flex justify-between text-primary-800">
+          <p className="font-semibold">Minimum Margin Ratio</p>
+          <p>{minMarginBps / 100}%</p>
         </div>
       </div>
     </div>
