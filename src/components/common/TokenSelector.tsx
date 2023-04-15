54447ffa37ade878b3b6a515b516549019a37f84
mangming2 <leejiho35@gmail.com>
2023-04-15 19:32:49 +0900

    Update : token image on trade

--- src/components/common/TokenSelector.tsx
+++ src/components/common/TokenSelector.tsx
@@ -1,36 +1,52 @@
 import { Token } from "modules/Token";
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useState } from "react";
+
+import usdc from "../../assets/token/usdcSvg.svg";
 
 interface TokenSelectorProps {
   tokenList: Token[];
   selected: string;
+  tokenImg: string;
+
   onSelect: (symbol: string) => void;
 }
 export const TokenSelector = ({
   tokenList,
   selected,
+  tokenImg,
   onSelect,
 }: TokenSelectorProps) => {
   const onOptionChanged: ChangeEventHandler<HTMLSelectElement> = ({
     target: { value },
   }) => {
     console.log("select", value);
+    const selectedOption = tokenList.find((option) => option.symbol === value);
+    if (selectedOption) {
+      setImgUrl(selectedOption.imgUrl);
+    } else {
+      setImgUrl("");
+    }
     onSelect(value);
   };
+  const [imgUrl, setImgUrl] = useState(tokenImg);
+
   return (
-    <div className="select-wrapper">
+    <div className="flex flex-row select-wrapper">
+      <img src={imgUrl} className="mr-2 w-8" />
       <select
         className=" appearance-none outline-none bg-transparent font-semibold border rounded pl-3 pr-7 pt-1.5 py-1"
         onChange={onOptionChanged}
       >
         {tokenList.map((t) => (
-          <option
-            key={t.symbol}
-            value={t.symbol}
-            selected={selected === t.symbol}
-          >
-            {t.symbol}
-          </option>
+          <>
+            <option
+              key={t.imgUrl}
+              value={t.symbol}
+              selected={selected === t.symbol}
+            >
+              {t.symbol}
+            </option>
+          </>
         ))}
       </select>
     </div>
--- src/modules/tokens.data.ts
+++ src/modules/tokens.data.ts
@@ -1,33 +1,36 @@
 import { CHAINID, TokenData } from "interfaces/config-data.interface";
+import usdc from "../assets/token/usdcSvg.svg";
+import doge from "../assets/token/dogeSvg.svg";
+import usdt from "../assets/token/usdtSvg.svg";
 
-const TOKENS:TokenData[] = [ 
-    {
-        chainId: CHAINID.LOCAL,
-        name: "USD Coin",
-        decimals: 18,
-        symbol: "USDC",
-        address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
-        isWrappedToken: false,
-        imgUrl: "",
-    },
-    {
-        chainId: CHAINID.LOCAL,
-        name: "DogeCoin",
-        decimals: 18,
-        symbol: "DOGE",
-        address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
-        isWrappedToken: false,
-        imgUrl: "",
-    },
-    {
-        chainId: CHAINID.LOCAL,
-        name: "Token3",
-        decimals: 18,
-        symbol: "T3",
-        address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
-        isWrappedToken: false,
-        imgUrl: "",
-    },
+const TOKENS: TokenData[] = [
+  {
+    chainId: CHAINID.LOCAL,
+    name: "USD Coin",
+    decimals: 18,
+    symbol: "USDC",
+    address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+    isWrappedToken: false,
+    imgUrl: usdc,
+  },
+  {
+    chainId: CHAINID.LOCAL,
+    name: "DogeCoin",
+    decimals: 18,
+    symbol: "DOGE",
+    address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+    isWrappedToken: false,
+    imgUrl: doge,
+  },
+  {
+    chainId: CHAINID.LOCAL,
+    name: "Token3",
+    decimals: 18,
+    symbol: "T3",
+    address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+    isWrappedToken: false,
+    imgUrl: usdt,
+  },
 ];
 
 export default TOKENS;
