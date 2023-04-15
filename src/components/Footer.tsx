b0cd78bc8cba7fcf0e1a8e1249586261a3a62ccc
Sooyoung Lee <dannylisa67@gmail.com>
2023-04-15 11:11:19 +0900

    deprecated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="px-8 h-[72px] bg-gray-800 text-primary-300 border-t border-t-neutral-200 flex items-center justify-end">
-      <div>@2023 DeFuture All rights reserved</div>
-    </footer>
+    // <footer className="px-8 h-[64px] text-primary-300 border-t border-t-neutral-200 flex items-center justify-end">
+    //   <div>@2023 DeFuture All rights reserved</div>
+    // </footer>
+    <footer></footer>
   );
 };
 
